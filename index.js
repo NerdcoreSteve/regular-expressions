a97c9890fe55567d127a7bc0062f780385397ae3
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-07-16 20:05:46 -0600

    added more examples

--- index.js
+++ index.js
@@ -36,3 +36,49 @@ console.log(
         .match(/[a-z]+/gi))
 
 console.log('[1, 2, 1 + 2]'.match(/[\[\+]+/g))
+
+console.log(
+    'Potatoes are my friends, potatoes are my family.'
+        .match(/^potatoes/gi))
+console.log(
+    'Family Ties is a good show, but I prefer All In the Family.'
+        .match(/family.$/gi))
+console.log(
+    'The Magic School Bus was a great show, but they really should have asked that kid\'s parents before driving through his digestive system'
+        .match(/bu/gi))
+console.log(
+    'The Magic School Bus was a great show, but they really should have asked that kid\'s parents before driving through his digestive system'
+        .match(/^bu$/gi))
+
+console.log(
+    'A baker\'s dozen is 13. Why is that?'
+        .match(/\d+/g))
+
+console.log(
+    'It was the best of times. It was the worst of times'
+        .match(/(best|worst)/g))
+
+console.log(
+    'Twas bryllyg, and ye slythy toves Did gyre and gymble in ye wabe'
+        .match(/(bryllyg|slythy|toves|gyre)/g))
+
+console.log(
+    'I get a banana. You get a kiwi. Your mom is a potato'
+        .match(/get a (\w+)/))
+
+console.log(
+    'I get a banana. You get a kiwi. Your mom is a potato'
+        .match(/get a (\w+)/g))
+
+const regexGlobalGroupCapture = (regex, input) => {
+    var matches, output = []
+    while (matches = regex.exec(input)) {
+        output.push(matches[1])
+    }
+    return output
+}
+
+console.log(
+    regexGlobalGroupCapture(
+        /get a (\w+)/g,
+        "I get a banana. You get a kiwi. Your mom is a potato"))
